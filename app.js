f2a35731b54ca7c3ddbb371ff9681855ba165070
johnykary <jonh.stef.kary@gmail.com>
2022-08-16 15:10:07 +0300

    418 status

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+app.get('/demo', (req, res)=>{
+  res.set('x-full-stack', '4life').status(418).send('I prefer coffee')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
